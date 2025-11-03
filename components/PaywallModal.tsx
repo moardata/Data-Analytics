ac01e246c6c6301c300219fae1c469fc0a738693
Tyler Mckenzie <tylermckenzie@Tylers-MBP.lan>
2025-11-03 12:40:29 +1000

    Revert: Remove companyId from upgrade link
    This was causing navigation to go to surveys instead of the upgrade page.
    Reverting to original behavior.

--- components/PaywallModal.tsx
+++ components/PaywallModal.tsx
@@ -235,7 +235,7 @@ export function PaywallModal({ isOpen, onClose, reason }: PaywallModalProps) {
                 {/* Link to see other plans */}
                 <div className="text-center">
                   <a
-                    href={`/upgrade?companyId=${companyId}`}
+                    href="/upgrade"
                     className="text-xs text-[#A1A1AA] hover:text-[#F8FAFC] transition-colors underline"
                   >
                     View all plans and pricing
